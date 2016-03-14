b48b2580a02985fe7cad6bfd03007d07f6a33038
Rocky <zhaidyan@gmail.com>
2016-03-14 17:01:26 +0800

    Revert "first"
    This reverts commit 11b588ab29a58df527840ed8c6173e3e9e19d187.

--- src/main/java/com/zdy/main/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.zdy.main;
-
-/**
- * Created by simple on 2015/7/29.
- */
-public class Constant {
-    public static final String KEY = "c41097d32d43664e4a6a0afa875c8e38";
-    public static final String SECRT = "SsBeXESwW43675ip_fF2vVqa41w3aZwX";
-}
